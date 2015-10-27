5b104dd97dae11366f39ccbac0494b3291113909
David DeGraw <ddegraw@LIN-CMS-DDEGRAW.digi.com>
2015-10-27 14:07:44 -0600

    Add basic emoji replacing

--- filter.js
+++ filter.js
@@ -1,43 +1,19 @@
 var wordList;
-var preserveFirst;
-var filterSubstring;
+var emojiList;
 var readyWordList = false;
-var readyPreserveFirst = false;
-var readyFilterSubstring = false;
 var profanityList = [];
 
 // Retrieve the localStorage from background page
 var port = chrome.extension.connect({name: "getLocalStorage"});
 port.postMessage({localStorage: "wordList"});
-port.postMessage({localStorage: "preserveFirst"});
 port.postMessage({localStorage: "filterSubstring"});
 port.onMessage.addListener(function(msg) {
   if (msg.wordList) {
 	wordList = msg.wordList.split(",");
-	if (readyPreserveFirst && readyFilterSubstring) {
-		// When all local storage retrieved, begin removing profanity
-		generateProfanityList();
-		removeProfanity();
-	}
+    emojiList = ["🐴⚫","🐩","🐓","©️","🌊","👉👌","⛲","💃","💩","🌀🌀","💃","👋"];
+	generateProfanityList();
+	removeProfanity();
 	readyWordList = true;
-  }
-  if (msg.preserveFirst) {
-	preserveFirst = (msg.preserveFirst == "true");
-	if (readyWordList && readyFilterSubstring) {
-		// When all local storage retrieved, begin removing profanity
-		generateProfanityList();
-		removeProfanity();
-	}
-	readyPreserveFirst = true;
-  }
-  if (msg.filterSubstring) {
-	filterSubstring = (msg.filterSubstring == "true");
-	if (readyWordList && readyPreserveFirst) {
-		// When all local storage retrieved, begin removing profanity
-		generateProfanityList();
-		removeProfanity();
-	}
-	readyFilterSubstring = true;
   }
 });
 
@@ -46,14 +22,8 @@ document.addEventListener('DOMNodeInserted', removeProfanityFromNode, false);
 
 // Parse the profanity list
 function generateProfanityList() {
-	if (filterSubstring) {
-		for (var x = 0; x < wordList.length; x++) {
-			profanityList.push(new RegExp("(" + wordList[x][0] + ")" + wordList[x].substring(1), "gi" ));
-		}
-	} else {
-		for (var x = 0; x < wordList.length; x++) {
-			profanityList.push(new RegExp("\\b(" + wordList[x][0] + ")" + wordList[x].substring(1) + "\\b", "gi" ));
-		}
+	for (var x = 0; x < wordList.length; x++) {
+		profanityList.push(new RegExp("(" + wordList[x][0] + ")" + wordList[x].substring(1), "gi" ));
 	}
 }
 
@@ -66,11 +36,11 @@ function removeProfanity() {
 		XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
 		null
 	);
-	
+
 	for (var i = 0; i < evalResult.snapshotLength; i++) {
 		var textNode = evalResult.snapshotItem(i);
 		for (var z = 0; z < profanityList.length; z++) {
-			textNode.data = textNode.data.replace(profanityList[z], starReplace);
+			textNode.data = textNode.data.replace(profanityList[z], emojiReplace);
 		}
 	}
 }
@@ -78,7 +48,7 @@ function removeProfanity() {
 // Remove the profanity from the node
 function removeProfanityFromNode(event) {
 	var node = event.target;
-	
+
 	var evalResult = document.evaluate(
 		'.//text()[normalize-space(.) != ""]',
 		node,
@@ -86,28 +56,20 @@ function removeProfanityFromNode(event) {
 		XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
 		null
 	);
-	
+
 	for (var i = 0; i < evalResult.snapshotLength; i++) {
 		var textNode = evalResult.snapshotItem(i);
 		for (var z = 0; z < profanityList.length; z++) {
-			textNode.data = textNode.data.replace(profanityList[z], starReplace);
+			textNode.data = textNode.data.replace(profanityList[z], emojiReplace);
 		}
 	}
 }
 
-// Replace the profanity with a string of asterisks
-function starReplace(strMatchingString, strFirstLetter) {
+// Replace the profanity with an emoji
+function emojiReplace(strMatchingString, strFirstLetter) {
 	var starString = "";
-	if (!preserveFirst) {
-		for (var i = 0; i < strMatchingString.length; i++) {
-			starString = starString + "*";
-		}
-	} else {
-		starString = strFirstLetter;
-		for (var i = 1; i < strMatchingString.length; i++) {
-			starString = starString + "*";
-		}
-	}
-	
+
+    starString = emojiList[wordList.indexOf(strMatchingString)];
+
 	return starString;
 }
---
+++ listener.js
@@ -0,0 +1,17 @@
+function begin() {
+	// Set default values
+	if (!localStorage.wordList) {
+		localStorage.wordList = "asshole,bitch,cock,cunt,damn,fuck,piss,slut,shit,tits,whore,hello";
+	}
+
+	// Handle Message Passing for localStorage
+	chrome.extension.onConnect.addListener(function(port) {
+	  port.onMessage.addListener(function(msg) {
+		if (msg.localStorage == "wordList") {
+		  port.postMessage({wordList: localStorage.wordList});
+		}
+	  });
+	});
+}
+
+window.addEventListener("DOMContentLoaded", begin);
